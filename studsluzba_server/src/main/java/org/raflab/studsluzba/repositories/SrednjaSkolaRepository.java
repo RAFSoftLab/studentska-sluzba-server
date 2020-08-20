2df0b265d3ed051f98c844d980c1f228141c6d0c
bojanads <bdimicsurla@raf.rs>
2020-08-20 11:13:30 +0200

    srednja skola sifarnik

---
+++ studsluzba_server/src/main/java/org/raflab/studsluzba/repositories/SrednjaSkolaRepository.java
@@ -0,0 +1,8 @@
+package org.raflab.studsluzba.repositories;
+
+import org.raflab.studsluzba.model.SrednjaSkola;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SrednjaSkolaRepository extends CrudRepository<SrednjaSkola, Long> {
+
+}
